363568a559c40316eacdd267fa72c0e9745819ce
riikakelvin <riikakelvin2@gmail.com>
2023-06-08 16:42:19 -0700

    commited task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,16 +2,8 @@
 if __name__=="__main__":
     import sys
 
-    arg = sys.argv
-    num_count = len(arg) - 1
+    add_total = 0
+    for x in range(len(sys.argv) - 1):
+       add_total = add_total + int(sys.argv[i + 1])
+    print("{}".format(add_total))
 
-    if num_count == 0:
-        print("{:d}".format(num_count))
-    
-    else:
-        n = 1
-        added = 0
-        while n <= num_count:
-            added = added + int(argv[n])
-            n += 1
-        print("{:d}".format(added))
