bf64100410c368c8864211833b824c9a9337c07c
riikakelvin <riikakelvin2@gmail.com>
2023-06-12 14:58:52 -0700

    commit for task 8 a function that returns a tuple with the length of a string and its first character.

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def no_c(my_string):
+        copy = [x for x in my_string if x != 'C' and x != 'c']
+    return (" ".join(copy))
+
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_k=(), tuple_w=()):
+        tuple_w += (0, 0)
+        tuple_k += (0, 0)
+    return(tuple_k[0] + tuple_w[0], tuple_k[1] + tuple_w[1])
+
