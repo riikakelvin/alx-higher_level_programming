4cd918d7a98a40d2cc2afd82c20046555e2d7187
riikakelvin <riikakelvin2@gmail.com>
2023-06-12 15:59:03 -0700

    commit for task 8 re-imagined

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
-    if sentence == " ":
-        return (0, None)
-    else:
-         return (len(sentence), sentence[0])
+    length = len(sentence)
+    first_character = sentence[0] if length > 0 else "None"
+    tupl = length, first_character
+    return(tupl)
