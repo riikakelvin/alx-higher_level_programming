7394b56ef4b92db594699cb9a5b7aea18843ec15
riikakelvin <riikakelvin2@gmail.com>
2023-06-14 14:04:48 -0700

    adjustmemts in code that treats strings not as ints, by inserting the len function to obtain the number of keys available in dictionary

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    num_keys = 0
-
     list_of_keys = list(a_dictionary.keys())
 
-    for i in list_of_keys:
-        num_keys += i
-
-    return (num_keys)
+    return (len(list_of_keys)
