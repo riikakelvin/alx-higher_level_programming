61cdd212d2972c530d489d12cf32c7fa357708f5
riikakelvin <riikakelvin2@gmail.com>
2023-06-14 13:15:51 -0700

    commit for task 2, a program that adds all unique integers in a list (only once for each integer).

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    outlier_list = set(my_list) #set disallows dupicates
+    integer = 0
+
+    for i in outlier_list:
+        integer+ = i
+
+    return (integer)
