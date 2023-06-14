02314b89371b1dfa849c2b712419f7bdef9ad136
riikakelvin <riikakelvin2@gmail.com>
2023-06-14 14:28:23 -0700

    task 6 xode updates, placing appropriate return

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@ def print_sorted_dictionary(a_dictionary):
     key_list_dictionary = list(a_dictionary.keys())
     key_list_dictionary.sort()
 
-    return sorted_dictionary
+    return key_list_dictionary
 
