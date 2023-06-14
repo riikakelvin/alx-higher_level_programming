fe4c4d42be4d882cc275889f9bd78bfa677cc71e
riikakelvin <riikakelvin2@gmail.com>
2023-06-14 14:25:25 -0700

    corrections in code for task 6 & 5., in 6, cporrect application of sort () function

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     
-    list_of_keys = list(len(a_dictionary.keys()))
+    list_of_keys = len(a_dictionary.keys())
 
-    return (list_of_keys)
+    return list_of_keys
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_dictionary = list(a_dictionary.sort())
+    key_list_dictionary = list(a_dictionary.keys())
+    key_list_dictionary.sort()
 
-    return (sorted_dictionary)
+    return sorted_dictionary
 
