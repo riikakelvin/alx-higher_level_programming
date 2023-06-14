5f41568ea53fa796e58f430ac5303d90bdfa6919
riikakelvin <riikakelvin2@gmail.com>
2023-06-14 16:45:07 -0700

    commit for task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
        if roman_string is None or type(roman_string) != str:
         return 0
-    Sample_data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sample_data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = [sample_data[x] for x in roman_string] + [0]
     conversion = 0
 
