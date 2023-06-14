4a72170fb317285b1351987381bc20713d5543bc
riikakelvin <riikakelvin2@gmail.com>
2023-06-14 13:53:05 -0700

    commit for task 5, a program that returns the number of keys in a dictionary.

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    num_keys = 0
+
+    list_of_keys = list(a_dictionary.keys())
+
+    for i in num_keys:
+        num_keys += i
+
+    return (num_keys)
