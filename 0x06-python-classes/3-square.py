2fb607e229733c98fcdd4137cbebe537cc572e67
riikakelvin <riikakelvin2@gmail.com>
2023-06-27 14:33:43 -0700

    commit for tested tasks for project 0x06. Python - Classes and Objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 class Square:
+    pass
     def __init__(self, size=0):
 
         if not isinstance(size, int):
-        raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
