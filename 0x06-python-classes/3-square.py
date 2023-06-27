0c735bfd727a0f1accc5c81f47ca07c91960fd2b
riikakelvin <riikakelvin2@gmail.com>
2023-06-27 14:10:08 -0700

    commit for tasks 3,4,5,6, all executable files now

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
-            
+        Args:
+            size (int):
         if not isinstance(size, int):
         raise TypeError("size must be an integer")
         elif size < 0:
