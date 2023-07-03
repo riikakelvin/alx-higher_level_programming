485ab350f90c5b428280d1db49c8b524b911f8a1
riikakelvin <riikakelvin2@gmail.com>
2023-07-03 16:16:16 -0700

    commit for 3. String representation, correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-'''Defines a Rectangle class.'''
+'''Defines a Rectangle class'''
 
 class Rectangle:
-    '''Representation of a rectangle.'''
+    '''Represents a rectangle'''
 
     def __init__(self, width=0, height=0):
-        '''Initializes a new Rectangle.'''
+        '''Initialize a new Rectangle.'''
 
         """Args:
-            width (int): new rectangle's width.
-            height (int): new rectangle's width.
+            width (int): new rectangle's width
+            height (int): new rectangle's height
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        '''Sets width of the Rectangle.'''
+        '''Sets width of the Rectangle'''
         return self.__width
 
     @width.setter
@@ -29,7 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the Rectangle."""
+        '''Sets height of the Rectangle'''
         return self.__height
 
     @height.setter
@@ -41,22 +41,22 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        '''Returns area computation of the Rectangle.'''
+        '''Return area of the Rectangle'''
         return (self.__width * self.__height)
 
     def perimeter(self):
-        '''Return the perimeter of the Rectangle.'''
+        '''Return perimeter of the Rectangle'''
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        '''Returns printable representation of the Rectangle.
+        """Returns printable representation of the Rectangle.
 
-        Represents the rectangle with the (hush)# character.
-        '''
+        Represents the rectangle with the # character.
+        """
         if self.__width == 0 or self.__height == 0:
-            return (" ")
+            return ("")
 
         rect = []
         for i in range(self.__height):
