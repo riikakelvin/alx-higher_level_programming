7cc76ea1138f93eaa414a84dd0737c6f264a1e43
riikakelvin <riikakelvin2@gmail.com>
2023-08-16 08:34:00 -0700

    commit for task 17, Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,16 @@
 -- uses the hbtn_0d_tvshows database to list all genres
--- unlkinked to DEXTER, display: tv_genres.name
-SELECT name FROM tv_genres
-WHERE tv_genres.id NOT IN (
-      SELECT tv_genres.id FROM tv_genres
-      JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-      JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
-      WHERE tv_shows.title = "DEXTER" )
+-- unlkinked to DEXTER, display: tv_genres.na
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN (
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "DEXTER"
+)
 --  sorted in ascending order by the genre name
 ORDER BY tv_genres.name;
 
