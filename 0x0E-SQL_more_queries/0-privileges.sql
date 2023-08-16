3c49fb44bb3b6ebfe9eaa34364606c16fb8b87b5
riikakelvin <riikakelvin2@gmail.com>
2023-08-16 09:04:38 -0700

    commit for task 0, privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- lists all privileges of the MySQL user_0d_1,
--- lists all privileges of the MySQL user_0d_2,
+-- lists all privileges of the MySQL user_0d_1
+-- lists all privileges of the MySQL user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
