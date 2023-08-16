6a2581837d2041bb8ba5bc16373d25cf83a554f2
riikakelvin <riikakelvin2@gmail.com>
2023-08-16 02:36:19 -0700

    commit for tasks 0 through to task 6

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- lists all privileges of the MySQL user_0d_1, in local server
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- lists all privileges of the MySQL user_0d_2, in local server
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates the MySQL server user user_0d_1, access pass: user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- gives all privileges to user user_0d_1
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
