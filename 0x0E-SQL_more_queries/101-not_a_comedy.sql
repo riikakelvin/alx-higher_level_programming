915beb17a2cb05861dcf16c759dfc55d1c352157
riikakelvin <riikakelvin2@gmail.com>
2023-08-16 08:37:02 -0700

    commit for task 18, No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,15 @@
 -- lists all shows without the genre Comedy, in hbtn_0d_tvshows. database
 -- Each record should display: tv_shows.title
-SELECT tv_shows.title FROM tv_shows
-WHERE tv_shows.id NOT IN (
-      SELECT tv_shows.id FROM tv_shows
-      JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-      JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
-      WHERE tv_genres.name = "Comedy" )
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN (
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy')
 --  sorted in ascending order by the show title
 ORDER BY tv_shows.title;
 
