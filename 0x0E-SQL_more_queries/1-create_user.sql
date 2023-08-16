ae037780e30776c954770ac08ec8893ad5746f3b
riikakelvin <riikakelvin2@gmail.com>
2023-08-16 09:08:07 -0700

    commit for task 1, Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,6 @@
 -- creates the MySQL server user user_0d_1
 -- access pass: user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 -- gives all privileges to user user_0d_1
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
 
