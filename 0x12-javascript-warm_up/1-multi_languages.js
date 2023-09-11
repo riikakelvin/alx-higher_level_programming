b9008cbd8fa52a8caf14bc4b9981bedb1a0fd929
riikakelvin <riikakelvin2@gmail.com>
2023-09-11 16:05:21 -0700

    commit for task 2, 2. Arguments

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const first_line = “C is fun”;
 const second_line = “Python is cool”;
-const third_line = “JavaScript is amazing”;
-console.log({ first_line, second_line, third_line});
+const third-line = “JavaScript is amazing”;
+console.log(first_line);
+console.log(second_line);
+console.log(third_line);
