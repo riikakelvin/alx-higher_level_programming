9f6ab70d85db2453434c0b3cd2f9142efe255915
riikakelvin <riikakelvin2@gmail.com>
2023-09-12 15:59:08 -0700

    commit for code for task 5, readjusted

--- 0x13-javascript_objects_scopes_closures/5-main.js
+++ 0x13-javascript_objects_scopes_closures/5-main.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
+const Square = require('./5-square');
 
-const Rectangle = require('./4-rectangle');
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-}
-
-module.exports = Square;
+const s1 = new Square(4);
+s1.print();
+s1.double();
+s1.print();
