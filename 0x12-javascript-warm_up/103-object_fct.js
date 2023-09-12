1dfd2187c8291f5281659b4f4cdeaa6d5caa0f3f
riikakelvin <riikakelvin2@gmail.com>
2023-09-11 17:10:28 -0700

    commit for task 17, adjusted script

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,13 +4,12 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-/*
-YOUR CODE HERE
-*/
+myObject.incr = function () {
+  this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
