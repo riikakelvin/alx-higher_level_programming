2d0614060040680e9d5d6ce49c8010a74ebac722
riikakelvin <riikakelvin2@gmail.com>
2023-09-12 15:41:39 -0700

    commit for task 3,

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
 	constructor (h, w) {
-		if (h <=0 || w <= 0) {
+		if (h > 0 && w > 0) {
 			this.height = h;
 			this.width = w;
 		}
 	}
 }
+module.exports = Rectangle;
