7ec702b5af361bbc1eaa035c29550c303205af6a
riikakelvin <riikakelvin2@gmail.com>
2023-09-16 13:18:48 -0700

    commit for task 1, adjusted code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""states models
+"""
 if __name__ == "__main__":
     import MySQLdb
     import sys
@@ -14,7 +16,8 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name \
+LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
